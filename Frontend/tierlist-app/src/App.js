6dd72635358f3152cc1079d7583a8901cba0aa90
JP Belval <jpbelv@gmail.com>
2023-03-08 23:01:54 -0500

    Init commit

---
+++ Frontend/tierlist-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './home/home-card';
+import HomeCard from './home/home-card';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomeCard></HomeCard>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/tierlist-app/src/home/home-card.js
@@ -0,0 +1,11 @@
+import styles from "./home-card.css";
+
+function HomeCard() {
+
+    return (
+      <div className="homeMainCard">
+      </div>
+    );
+}
+
+export default HomeCard;
